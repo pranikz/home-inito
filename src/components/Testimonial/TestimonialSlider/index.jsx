fcdab2910816a244905b16b8be8196902c434729
pranikz <pratyushmahapatra2000@gamil.com>
2024-07-17 23:50:48 +0530

    chore: minor Fixes

--- src/components/Testimonial/TestimonialSlider/index.jsx
+++ src/components/Testimonial/TestimonialSlider/index.jsx
@@ -7,27 +7,32 @@ import 'slick-carousel/slick/slick-theme.css';
 const index = () => {
 
 
+
+
     const settings = {
         dots: true,
         arrows: false,
     };
     return (
-        <div className=" m-0 ml-auto">
-            <Slider {...settings}>
-                <div>
-                    <img src="http://placehold.co/600x400" />
-                </div>
-                <div>
-                    <img src="http://placehold.co/600x400" />
-                </div>
-                <div>
-                    <img src="http://placehold.co/600x400" />
-                </div>
-                <div>
-                    <img src="http://placehold.co/600x400" />
-                </div>
-            </Slider>
+        <div className='mx-auto  lg:max-w-6xl items-center justify-between lg:p-6 lg:px-8 flex-col pb-10'>
+            <div className=" m-0 ml-auto ">
+                <Slider {...settings}>
+                    <div className='rounded w-full max-w-6xl shadow-sm h-40'>
+
+                    </div>
+                    <div className='rounded w-full max-w-6xl shadow-sm h-40'>
+
+                    </div>
+                    <div className='rounded w-full max-w-6xl shadow-sm h-40'>
+
+                    </div>
+                    <div className='rounded w-full max-w-6xl shadow-sm h-40'>
+
+                    </div>
+                </Slider>
+            </div>
         </div>
+
     );
 }
 
